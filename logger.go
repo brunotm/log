39fac2fc72e1ad3ecd39098e3d43c247a1b66d62
Bruno Moura <brunotm@gmail.com>
2021-04-07 17:53:31 +0100

    add package default logger

---
+++ logger.go
@@ -0,0 +1,50 @@
+package log
+
+import (
+	"bufio"
+	"os"
+)
+
+var logger *Logger
+
+func init() {
+	config := DefaultConfig
+	config.CallerSkip++
+	logger = New(bufio.NewWriter(os.Stderr), DefaultConfig)
+}
+
+// SetFormat sets the logging format for the default package logger
+func SetFormat(f Format) {
+	logger.SetFormat(f)
+}
+
+// SetLevel sets the logging level for the default package logger
+func SetLevel(l Level) {
+	logger.SetLevel(l)
+}
+
+// Debug creates a new log entry with the given message with the default package logger.
+func Debug(message string) (entry Entry) {
+	return logger.Debug(message)
+}
+
+// Info creates a new log entry with the given message with the default package logger.
+func Info(message string) (entry Entry) {
+	return logger.Info(message)
+}
+
+// Warn creates a new log entry with the given message with the default package logger.
+func Warn(message string) (entry Entry) {
+	return logger.Warn(message)
+}
+
+// Error creates a new log entry with the given message with the default package logger.
+func Error(message string) (entry Entry) {
+	return logger.Error(message)
+}
+
+// Fatal creates a new log entry with the given message with the default package logger.
+// After write, Fatal calls os.Exit(1) terminating the running program
+func Fatal(message string) (entry Entry) {
+	return logger.Fatal(message)
+}
